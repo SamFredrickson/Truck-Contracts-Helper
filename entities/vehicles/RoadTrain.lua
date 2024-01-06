4edfb7dea0264e8a25c6facb34909810c6bee031
Sam <samfredrickson041@gmail.com>
2024-01-06 21:06:05 +0300

    fix: указать верный ид модели для машины

--- entities/vehicles/RoadTrain.lua
+++ entities/vehicles/RoadTrain.lua
@@ -7,7 +7,7 @@ local Vehicle = require "tch.entities.vehicles.vehicle"
 
 local RoadTrain = {
     new = function()
-        local self = Vehicle.new(514)
+        local self = Vehicle.new(515)
         return self
     end
 }
