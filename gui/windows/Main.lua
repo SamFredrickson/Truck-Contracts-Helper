ed4aa306a778ca719a2fec884659347d42020a5a
Sam <samfredrickson041@gmail.com>
2024-02-13 00:39:00 +0300

    refactor(Main.lua): убрать избыточные кнопки

--- gui/windows/Main.lua
+++ gui/windows/Main.lua
@@ -14,11 +14,11 @@ local Main = {
     new = function()
         local self = Window.new()
 
-        local sizes = imgui.ImVec2(410, 370)
-        local posX, posY = getScreenResolution()
-
         self.hideCursor = true
         self.contracts = {}
+
+        local sizes = imgui.ImVec2(410, 370)
+        local posX, posY = getScreenResolution()
 
         imgui.OnFrame(
             function() return self.window[0] end,
@@ -30,7 +30,6 @@ local Main = {
               player.HideCursor = self.hideCursor
 
               for number, contract in ipairs(self.contracts) do
-
                 local headerTitle = string.format(
                     "%s%d. %s -> %s[%d / %d]", 
                     contract.top and "[TOP] " or "",
@@ -40,7 +39,6 @@ local Main = {
                     contract.amount.first,
                     contract.amount.second
                 )
-
                 if imgui.CollapsingHeader(u8(headerTitle)) then
                     if imgui.Button(string.format(u8"Взять контракт ##%d", contract.id)) and MenuDialogue.isTakingAllowed(self.window[0]) then
                        MenuDialogue.take(contract.id)
@@ -62,44 +60,8 @@ local Main = {
                     if imgui.Button(string.format(u8"Отм ##%d", contract.id)) then
                         MenuDialogue.cancel()
                     end
-                    if imgui.Button(string.format(u8"Доступен контракт (/j) ##%d", contract.id)) then
-                        local message = string.format(
-                            "/j Внимание! Доступен контракт %d. %s -> %s [%d / %d]",
-                            contract.id, 
-                            contract.source, 
-                            contract.destination,
-                            contract.amount.first,
-                            contract.amount.second
-                        )
-                        sampSendChat(message)
-                    end
-                    imgui.SameLine()
-                    if imgui.Button(string.format(u8"Советую (/j) ##%d", contract.id)) then
-                        local message = string.format(
-                            "/j Советую взять %d. %s -> %s [%d / %d]",
-                            contract.id, 
-                            contract.source, 
-                            contract.destination,
-                            contract.amount.first,
-                            contract.amount.second
-                        )
-                        sampSendChat(message)
-                    end
-                    imgui.SameLine()
-                    if imgui.Button(string.format(u8"Остаток (/j) ##%d", contract.id)) then
-                        local message = string.format(
-                            "/j У контракта %d. %s -> %s осталось %d из %d груза",
-                            contract.id, 
-                            contract.source, 
-                            contract.destination,
-                            contract.amount.first,
-                            contract.amount.second
-                        )
-                        sampSendChat(message)
-                    end
                 end
               end
-
               imgui.End()
             end
         )
