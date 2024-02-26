76de10ea933ed799e19c87e11118d6a645dc9384
Sam <samfredrickson041@gmail.com>
2024-02-26 03:07:50 +0300

    feat(tch-main.lua): добавить звук появления новых координат

--- entities/sounds/Sound.lua
+++ entities/sounds/Sound.lua
@@ -1,16 +1,22 @@
 local AudioStreamState = require("moonloader").audiostream_state
 
 local Sound = {
-    new = function(name)
+    new = function(name, volume)
         local self = {}
         self.workingDirectory = getWorkingDirectory()
-        
+        self.volume = volume
+
         self.sound = loadAudioStream(
             string.format(
                 "%s/tch/resources/%s",
                 self.workingDirectory,
                 name
             )
+        )
+
+        setAudioStreamVolume(
+            self.sound,
+            self.volume
         )
 
         self.play = function()
